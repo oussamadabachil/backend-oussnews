6b079ee2cb00147849a3214a9390368bda007fd0
OussamaDabachil <oussamadabachilpro@gmail.com>
2023-01-17 15:35:12 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.render('users', { title: 'Users' });
+  res.render("users", { title: "Users" });
 });
 
 router.post("/inscription", (req, res) => {
