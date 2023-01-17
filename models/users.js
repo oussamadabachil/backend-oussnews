181ae495e014c88252449820ef443b423f1c093e
OussamaDabachil <oussamadabachilpro@gmail.com>
2023-01-17 15:16:29 +0100

    push origin maion

---
+++ models/users.js
@@ -0,0 +1,14 @@
+
+require('../models/connection')
+const mongoose = require ('mongoose')
+
+const userSchema = mongoose.Schema({
+    username:String,
+    password:String,
+    token:String
+})
+
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User;
---
+++ routes/index.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var router = express.Router();
+
+require("../models/connection")
+const User = require("../models/users")
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.post("/inscription",(req,res)=>{
+  if(req.body.username&&req.body.password){
+  User.findOne({username:req.body.username}).then(data=>{
+    if(data){
+      res.json({
+        message:"Le nom d'utilisateur est déja pris",
+        result:false
+      })
+    }else{
+      const newUser =  new User({
+        username:req.body.username,
+        password: req.body.password
+      })
+      newUser.save()
+      res.json({
+        message:'Vous vous êtes bien inscrit',
+        result:true,
+        newUser: newUser
+      })
+    }
+  })}
+  else{
+      res.json({
+        message:"Remplissez les champs vides"
+      })
+  
+    }
+
+})
+router.post("/connexion",(req,res)=>{
+
+
+  if(req.body.username&&req.body.password){
+    User.findOne({username:req.body.username,password:req.body.password}).then(data=>{
+      if(data){
+        const newUser =  new User({
+          username:req.body.username,
+          password: req.body.password
+      })
+      res.json({
+        result:true,
+        message:'Bienvenue',
+        user:newUser,
+      })
+    }else{
+      res.json({
+        result:false,
+        message:'Mot de passe incorrect'
+      })
+
+    }
+  })
+}else{
+
+  res.json({
+    data:'vide',
+    message:'Remplissez les champs'
+
+
+  })
+
+}
+
+    
+
+
+})
+
+
+
+
+
+
+module.exports = router;
